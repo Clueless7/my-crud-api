fd301fe588aac55303a33f554582d6833f39fc64
Josel <catalanjosel123@gmail.com>
2019-11-10 14:11:44 +0800

    added cors package

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const hobbiesRouter = require('./routes/api/hobbies');
+const cors = require('cors');
 const path = require('path');
+const hobbiesRouter = require('./routes/api/hobbies');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
